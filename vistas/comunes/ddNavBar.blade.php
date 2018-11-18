c616f0d7bdc19be319ee5b71f43e5c42ecae0581
Rommel <rommelmontoya97@gmail.com>
2018-11-18 17:43:35 -0400

    correccion boton navbar a pagina por defecto de cursos

--- vistas/comunes/ddNavBar.blade.php
+++ vistas/comunes/ddNavBar.blade.php
@@ -1,31 +1,31 @@
 <div class="dropdown">
 
-  	<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownNavBar" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo Funciones::PGSC('usuario');?>
+  	<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownNavBar" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{Funciones::PGSC('usuario')}}
   	</button>
 
 	<div class="dropdown-menu" aria-labelledby="dropdownNavBar">
 		
 		<a align="center" class="dropdown-item" href="./?mod=principal/principal">Principal</a>
 
-		<?php if((Funciones::PGSC('permisos')[0]=='1') or (Funciones::PGSC('permisos')[1]=='1')):?>
+		@if((Funciones::PGSC('permisos')[0]=='1') or (Funciones::PGSC('permisos')[1]=='1'))
 		<a align="center" class="dropdown-item" href="./?mod=login/usuarios">Usuarios</a>
-		<?php endif ?>
+		@endif
 	
-		<?php if((Funciones::PGSC('permisos')[0]=='1') or (Funciones::PGSC('permisos')[1]=='1')):?>
+		@if((Funciones::PGSC('permisos')[0]=='1') or (Funciones::PGSC('permisos')[1]=='1'))
 		<a align="center" class="dropdown-item" href="./?mod=estudiantes/estudiantes">Estudiantes</a>
-		<?php endif ?>
+		@endif
 
-		<?php if((Funciones::PGSC('permisos')[0]=='1') or (Funciones::PGSC('permisos')[1]=='1') or (Funciones::PGSC('permisos')[2]=='1')):?>
+		@if((Funciones::PGSC('permisos')[0]=='1') or (Funciones::PGSC('permisos')[1]=='1') or (Funciones::PGSC('permisos')[2]=='1'))
 		<a align="center" class="dropdown-item" href="./?mod=afiliados/afiliados">Afiliados</a>
-		<?php endif ?>
+		@endif
 
-		<?php if((Funciones::PGSC('permisos')[0]=='1') or (Funciones::PGSC('permisos')[1]=='1') or (Funciones::PGSC('permisos')[2]=='1') or (Funciones::PGSC('permisos')[3]=='1')):?>
-		<a align="center" class="dropdown-item" href="./?mod=cursos/cursos">Cursos</a>
-		<?php endif ?>
+		@if((Funciones::PGSC('permisos')[0]=='1') or (Funciones::PGSC('permisos')[1]=='1') or (Funciones::PGSC('permisos')[2]=='1') or (Funciones::PGSC('permisos')[3]=='1'))
+		<a align="center" class="dropdown-item" href="./?mod=cursos/crear_curso">Cursos</a>
+		@endif
 
-		<?php if(Funciones::PGSC('permisos')[3]=='1'):?>
+		@if(Funciones::PGSC('permisos')[3]=='1')
 		<a align="center" class="dropdown-item" href="./?mod=cursos/consultap1">Mis Cursos</a>
-		<?php endif ?>
+		@endif
 	
 		<a align="center" class="dropdown-item" href="./?mod=login/cerrarSesion">Cerrar Sesión</a>
 
